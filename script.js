2255b833f1df141fe1bd9cbd29f6143e3d28bb4a
Francisco Palacios <franciscopalacios10@yahoo.com>
2021-12-16 14:57:44 -0700

    equal and operator functions roughly  working

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+let displayValue = "";
+let displayElement = document.getElementById("display");
+let numberButtons = document.querySelectorAll(".number");
+let operatorButtons = document.querySelectorAll(".operator");
+let equalButton = document.getElementById("equal");
+let equation;
+let isFirstNumber = true;
+let clearNumber = false
+let operatorLookup = {
+	"add" : "+",
+	"subtract" : "-",
+	"multiply" : "*",
+	"divide" : "/"
+}
+let operator;
+let firstNumber;
+
 function add(a,b) {
 	return a + b;
 }
@@ -31,4 +49,47 @@ function operate(str, a, b) {
 }
 
 
-console.log( operate("*", 5, 5))
+function showResultInDisplay(res) {
+	firstNumber = res;
+
+	displayElement.value =  res;
+}
+// clicking number buttons updates display
+function updateDisplayValue(number) {
+	// clear numb
+	if (clearNumber){
+		displayValue = "";
+		clearNumber = false;
+	}
+
+	displayValue = `${displayValue}${number}`;
+	displayElement.value = displayValue
+}
+numberButtons.forEach(button => {
+	button.onclick = (el) => {
+		updateDisplayValue(el.target.innerHTML)
+	}
+});
+
+// clicking operator button saves first value and sets operator
+operatorButtons.forEach(button => {
+	button.onclick = (el) => {
+		// set the operator
+		operator =  operatorLookup[el.target.id];
+		
+
+		if (isFirstNumber) {
+			firstNumber = Number(displayValue);
+			isFirstNumber =  false;
+		} else {
+			showResultInDisplay(operate(operator, firstNumber,Number(displayValue)));
+		}
+		// clear display
+		clearNumber = true;
+	}
+});
+
+// solve equation
+equalButton.onclick = () => {
+	showResultInDisplay(operate(operator, firstNumber,Number(displayValue)));
+}
